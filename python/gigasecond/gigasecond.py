f19e7ddd65019d5b6fc7bd9a4ad7e9045efb09d8
Thom Suijkerbuijk <thomkrbk@live.nl>
2019-01-13 17:05:10 +0100

    added comments to gigasecond.py

--- python/gigasecond/gigasecond.py
+++ python/gigasecond/gigasecond.py
@@ -1,5 +1,9 @@
 from datetime import timedelta
 
 def add_gigasecond(moment):
+    """
+    add a gigasecond to a specific moment 
+    returns the new moment with the gigasecond added
+    """
     time = moment + timedelta(seconds=1000000000)
     return time
