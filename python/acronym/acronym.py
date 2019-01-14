9736ad4830a51631505ae5c02c603aec1c24607e
Thom Suijkerbuijk <thomkrbk@live.nl>
2019-01-14 11:48:51 +0100

    exercise acronym done

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,2 +1,11 @@
+import re
+
+
 def abbreviate(words):
-    pass
+    """
+    :param words: the word you want the abbreviation of
+    :return: returns the abbreviate as a string
+    """
+    list_abbreviate = re.split("[, \-!?:]+", words) # split on all given chars
+    abbreviate = [word[0].upper() for word in list_abbreviate] # take the first letter of each word
+    return ''.join(abbreviate)
