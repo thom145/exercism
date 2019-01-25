9ce3f871309a7f7e46c9310fa431cd99ed9c48d3
Thom Suijkerbuijk <thomkrbk@live.nl>
2019-01-25 16:34:56 +0100

    cleaned code for matrix solution

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,20 +1,15 @@
 class Matrix(object):
-    return_list = []
 
     def __init__(self, matrix_string):
-        self.matrix_string = matrix_string
+        self.matrix_string = matrix_string.split("\n")
 
     def column(self, index):
         index -= 1 # index of row starts at 1
 
-        for list_of_numbers in self.matrix_string.split("\n"):
-            self.return_list.append(int(list_of_numbers.split()[index]))
-        return self.return_list
+        return [int(list_of_numbers.split()[index]) for list_of_numbers in self.matrix_string]
 
     def row(self, index):
         index -= 1 # index of column starts at 1
 
-        for list_of_numbers in self.matrix_string.split("\n"):
-            row = [int(number) for number in list_of_numbers.split()]
-            self.return_list.append(row)
-        return self.return_list[index]
+        return_list = [int(i) for i in self.matrix_string[index].split(' ')]
+        return return_list
