430715ecaa9d5d1ec5e3740ec7d709df89b7789d
Thom Suijkerbuijk <thomkrbk@live.nl>
2019-02-06 20:02:20 +0100

    removed for loop - Word Count

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,16 +1,8 @@
 import re
-
+import collections
 
 def word_count(phrase):
     phrase_cleaned = re.sub(r'[?|$|.|!|,|:|%|^|&|@|_]', r' ', phrase) # removing all special char
-    phrase_cleaned = [word.strip('\'"') for word in phrase_cleaned.split()] # removing quotes
+    phrase_cleaned = [word.strip('\'"').lower() for word in phrase_cleaned.split()] # removing quotes
 
-    all_words = {}
-
-    for word in phrase_cleaned:
-        if word.lower() not in all_words.keys():
-            all_words[word.lower()] = 1
-        else:
-            all_words[word.lower()] += 1
-
-    return all_words
+    return dict(collections.Counter(phrase_cleaned))
