9e4fc64e859e841422a85ef8d5159030982b0217
Thom Suijkerbuijk <thomkrbk@live.nl>
2019-02-06 10:31:25 +0100

    Cleaned code for Word Count

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -2,9 +2,9 @@ import re
 
 
 def word_count(phrase):
-    phrase_cleaned = re.sub(r'[?|$|.|!|,|:|%|^|&|@|_]',r' ',phrase)
-    phrase_cleaned = [word.strip('\'"') for word in phrase_cleaned.split()]
-    print(phrase_cleaned)
+    phrase_cleaned = re.sub(r'[?|$|.|!|,|:|%|^|&|@|_]', r' ', phrase) # removing all special char
+    phrase_cleaned = [word.strip('\'"') for word in phrase_cleaned.split()] # removing quotes
+
     all_words = {}
 
     for word in phrase_cleaned:
@@ -12,4 +12,5 @@ def word_count(phrase):
             all_words[word.lower()] = 1
         else:
             all_words[word.lower()] += 1
+
     return all_words
