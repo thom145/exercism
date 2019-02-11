9b9e054d990b8d0f6c0722e85cb6ebc802ebede9
Thom Suijkerbuijk <thomkrbk@live.nl>
2019-02-11 12:27:17 +0100

    finished exercise triangle

---
+++ python/triangle/triangle.py
@@ -0,0 +1,35 @@
+def is_equilateral(sides):
+    if check_if_triangle(sides):
+        for side in sides:
+            if side == 0:
+                return False
+            else:
+                return len({side for side in sides}) == 1
+    else:
+        return False
+
+def is_isosceles(sides):
+    if check_if_triangle(sides):
+        for side in sides:
+            if side == 0:
+                return False
+            else:
+                return len({side for side in sides}) <= 2
+    else:
+        return False
+
+def is_scalene(sides):
+    if check_if_triangle(sides):
+        for side in sides:
+            if side == 0:
+                return False
+            else:
+                return len({side for side in sides}) == 3
+    else:
+        return False
+    
+def check_if_triangle(sides):
+    if sides[0] + sides[1] > sides[2] and sides[0] + sides[2] > sides[1] and sides[1] + sides[2] > sides[0]:
+        return True
+    else:
+        return False
