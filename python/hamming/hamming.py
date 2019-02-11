3ce1f55effc779b98d00baf280ad27fc0c81ba50
Thom Suijkerbuijk <thomkrbk@live.nl>
2019-02-11 11:39:06 +0100

    hamming more sufficient

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,12 +1,6 @@
 def distance(strand_a, strand_b):
     if len(strand_a) == len(strand_b):
-        if strand_a == strand_b:
-            return 0
-        else:
-            count = 0
-            for countA in range(len(strand_a)):
-                if strand_a[countA] != strand_b[countA]:
-                    count += 1
-            return count
+        return sum([1 for countA in range(len(strand_a)) if strand_a[countA] != strand_b[countA]])
+
     else:
         raise ValueError("Strands aren't of the same length")
